47c8f44d083223d636086418ce2d1229150d5da9
Saksham <121371714+saksham851@users.noreply.github.com>
2024-06-03 16:03:29 +0530

    cloudinary file upload work

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,31 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs   from 'fs';    
+
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret
+});
+
+const uploadOnCloudinary=asyn(localFilePath)
+{
+    try {
+        if(!localFilePath)return null
+        //upload on cloudinary
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file has been uploaded successfully.
+          return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) //removed the locally saved fie temporary file as the 
+        //upload operation got failed
+        return null;
+    }
+}
+
+cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
+  { public_id: "olympic_flag" }, 
+  function(error, result) {console.log(result); });
+
+  export {cloudinary};
